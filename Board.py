14dbc3f2914c3eb29278b4e936c3b75c1270b49b
rady.y <rady.y@passerellesnumeriques.org>
2020-11-06 17:14:23 +0700

    Create board and test

--- Board.py
+++ Board.py
@@ -1,10 +1,18 @@
-def boardGame():
-    BOARD_ROWS = 6
-    BOARD_COLUMNS = 7
-    result = ""
-    for row in range(BOARD_ROWS):
-        for column in range(BOARD_COLUMNS):
-            result += "0 "
-        result += "\n"
-    return result
+
+def hasDiskOnCell (disksPositions, cellX, cellY):
+    hasDisk = False
+    if (cellX > 5 and cellY > 5) and (cellX < 0 and cellY < 0):
+        hasDisk = False
+    else: 
+        for disk in disksPositions:
+            if disk[0] == cellX and disk[1] == cellY :
+                hasDisk = True
+    return hasDisk
+
+
+def getPlayerStartFirst(message):
+    hasStart = False
+    if message.upper() == "R":
+        hasStart = True
+    return hasStart
 
--- GameConsole.py
+++ GameConsole.py
@@ -1,6 +1,47 @@
 import Board
 
 def main():
-   return Board.boardGame()
 
-print(main())
+    whoStartFirst = str(input("Who start first? (R/Y) : "))
+    start = Board.getPlayerStartFirst(whoStartFirst)
+
+    isWin = True
+    isPlayed = False
+    boardColumnHeader = "\nA B C D E F G \n"
+    boardWelcomeMessage = "\nWELCOME TO DISK-BATTLE!\n"
+    boardUpdates = []
+    while isWin:
+        
+
+        if start and not isPlayed:
+            playerInput = eval(input("Player RED, Enter column to play: "))
+            isPlayed = True
+
+        else:
+            playerInput = eval(input("Player YELLOW, Enter column to play: "))
+            isPlayed = False
+            start = True
+        
+        
+        boardUpdates.append(playerInput)
+
+        # Write your code here !
+        result=""
+        BOARD_ROWS = 6
+        BOARD_COLUMNS = 7
+
+        for row in range(BOARD_ROWS):
+            for column in range(BOARD_COLUMNS):
+                if Board.hasDiskOnCell(boardUpdates, row, column):
+                    character = "R "
+        
+                else:
+                    character = "0 "
+                
+                result+=character
+            result+="\n"
+
+        print(boardWelcomeMessage + boardColumnHeader + result)   
+
+
+main()
